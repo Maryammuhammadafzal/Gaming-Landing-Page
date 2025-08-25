021df44f9a2d2b05e43311dd0c54cc3ff76b65e5
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-25 14:58:55 +0500

    created awards section

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -11,7 +11,7 @@ const FeaturesPage = () => {
     ]
 
     return (
-        <div className='w-full h-auto flex justify-center items-center px-3 lg:py-30 md:py-24 sm:py-18 xs:py-12 py-8'>
+        <div className='w-[95%] mx-auto h-auto flex justify-center items-center px-3 lg:py-30 md:py-24 sm:py-18 xs:py-12 py-8'>
             {/* Content */}
             <div className='md:w-1/2 w-full h-full flex flex-col gap-3 md:pl-4 xs:pl-8 pt-4'>
                 <h2 className='font-mono xl:text-[40px] lg:text-4xl md:text-3xl xs:text-2xl text-xl xl:max-w-lg md:max-w-md max-w-xl md:pr-0 xs:pr-20'>
@@ -19,10 +19,10 @@ const FeaturesPage = () => {
                 </h2>
                 <p className='lg:text-lg md:text-base text-sm uppercase'>From high-octane FPS titles to sprawling MMORPGs, we fuse speed, design, and strategy to redefine the way you play.</p>
 
-                <ul className='mt-4'>
+                <ul className='mt-6 flex gap-6 flex-col'>
                     {
                         featuresData.map((feature, index) => (
-                            <li key={index} className={`${index % 2 === 0 ? 'pl-10' : 'pl-0'} uppercase w-fit rounded-4xl px-8 py-5 bg-gradient-to-r from-[#FF6F00] to-[#FB8A33] text-secondary font-semibold`}>{feature}</li>
+                            <li key={index} className={`${index % 2 !== 0 ? 'ml-16' : 'ml-0'} uppercase rounded-4xl  w-[350px] text-center py-5 text-lg bg-gradient-to-r from-[#FF6F00] to-[#FB8A33] text-secondary font-semibold`}>{feature}</li>
                         ))
                     }
                 </ul>
@@ -30,11 +30,11 @@ const FeaturesPage = () => {
 
             {/* Image */}
             <div className='md:w-1/2 w-full md:pt-0 pt-20 relative h-full flex justify-center items-center'>
-                <div className='absolute w-full h-full flex justify-center items-center bottom-12'>
-                    <Image src='/images/feature-vector.png' alt='image' width={634} height={634} />
+                <div className='absolute w-full h-full flex justify-center items-center bottom-20'>
+                    <Image src='/images/feature-vector.png' alt='image' width={400} height={400} />
                 </div>
                 <div className='w-full h-full flex justify-center items-center z-10'>
-                    <Image src='/images/feature-image.png' alt='image' width={680} height={580} />
+                    <Image src='/images/feature-image.png' alt='image' width={430} height={540} />
                 </div>
             </div>
         </div>
